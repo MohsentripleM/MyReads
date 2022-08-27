df89a90415662c0220df0c29b4e796b7a61ac3ed
Mohsen <33673644+MohsentripleM@users.noreply.github.com>
2022-08-27 19:10:06 +0200

    first commit

---
+++ starter/src/components/Header.js
@@ -0,0 +1,12 @@
+import react from "react";
+import "..//App.css";
+
+const Header = () => {
+  return (
+    <div className="list-books-title">
+      <h1>MyReads</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ starter/src/components/Shelves.js
@@ -0,0 +1,28 @@
+import react from "react";
+import Shelf from "./Shelf";
+
+const Shelves = ({ books, changeShelf }) => {
+  const currentlyReading = books.filter(
+    (book) => book.shelf === "currentlyReading"
+  );
+  const wantToRead = books.filter((book) => book.shelf === "wantToRead");
+  const read = books.filter((book) => book.shelf === "read");
+
+  return (
+    <div>
+      <Shelf
+        title="Currently Reading"
+        books={currentlyReading}
+        changeShelf={changeShelf}
+      />
+      <Shelf
+        title="Want To Read"
+        books={wantToRead}
+        changeShelf={changeShelf}
+      />
+      <Shelf title="Read" books={read} changeShelf={changeShelf} />
+    </div>
+  );
+};
+
+export default Shelves;
