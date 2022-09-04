17f98007257ec0dbd2b31a8810c4684bad2a33a9
Mohsen <33673644+MohsentripleM@users.noreply.github.com>
2022-09-04 03:40:24 +0200

    list error fixiation

--- starter/src/components/Book.js
+++ starter/src/components/Book.js
@@ -1,36 +1,34 @@
 const Book = ({ book, changeShelf }) => {
   return (
     <div>
-      <li>
-        <div className="book">
-          <div className="book-top">
-            <div
-              className="book-cover"
-              style={{
-                width: 128,
-                height: 193,
-                backgroundImage: book.imageLinks
-                  ? `url(${book.imageLinks.thumbnail})`
-                  : "none",
-              }}
-            ></div>
-            <div className="book-shelf-changer">
-              <select
-                defaultValue={book.shelf ? book.shelf : "none"}
-                onChange={(e) => changeShelf(book, e.target.value)}
-              >
-                <option disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: book.imageLinks
+                ? `url(${book.imageLinks.thumbnail})`
+                : "none",
+            }}
+          ></div>
+          <div className="book-shelf-changer">
+            <select
+              defaultValue={book.shelf ? book.shelf : "none"}
+              onChange={(e) => changeShelf(book, e.target.value)}
+            >
+              <option disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
           </div>
-          <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors ? book.authors : ""}</div>
         </div>
-      </li>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors ? book.authors : ""}</div>
+      </div>
     </div>
   );
 };
